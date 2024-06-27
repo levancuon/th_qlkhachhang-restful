6c0f5da25a77f7410aa9395dcf085677fb1d6f60
levancuon <levancuongprovip@gmail.com>
2024-06-27 15:40:07 +0700

    ădnawo

---
+++ src/main/java/org/example/repository/ICustomerRepository.java
@@ -0,0 +1,10 @@
+package org.example.repository;
+
+
+import org.example.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/org/example/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+
+import org.example.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
